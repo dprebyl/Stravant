07125c3227ae9c970297f1ff66afef84ab6b9ac9
dprebyl <drakeprebyl@gmail.com>
2022-04-21 16:06:27 -0500

    More GPX processing

--- upload.php
+++ upload.php
@@ -1,7 +1,6 @@
 <?php
     require "db.php";
     
-
     $file = $_FILES["file"];
     $filename = $file["name"];
 
@@ -11,7 +10,25 @@
     }
 
     $gpx = simplexml_load_file($file["tmp_name"]) or die("Failed to load file");
-
     // TODO: Parse the GPX, insert into database, and redirect to view.php for the new activity
-    var_dump($gpx);
+	
+	$start = strtotime($gpx->metadata->time); // Time activity started
+	$track = $gpx->trk;
+	$name = $track->name; // Name of activity
+	$type = $track->type; // e.g. cycling (TODO: maybe make this a category automatically?)
+	
+	$points = []; // Array to contain all points
+	foreach ($track->trkseg->trkpt as $point) {
+		$lat = floatval($point["lat"]);
+		$lon = floatval($point["lon"]);
+		$ele = floatval($point->ele);
+		$time = strtotime($point->time);
+		$points[] = [$time, $lat, $lon, $ele];
+	}
+	
+	// TODO: Finish and test query
+	// $db->query("INSERT INTO activity (username, start, name, track) VALUES (?, ?, ?, ?)", [$_SESSION["username"], $start, $name, json_encode($points)]);
+	
+	// TODO: Get the primary key back
+	//header("Location: view.php?activity=");
 ?>
