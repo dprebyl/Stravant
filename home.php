b24f46de4b0666ca0e72488d0c747b8ad82c53e1
Reece Mathews <mathewsreece@gmail.com>
2022-04-27 21:00:42 -0500

    remove debug stuff >.<

--- home.php
+++ home.php
@@ -2,9 +2,6 @@
 <?php
 	require "db.php";
 	ensure_logged_in();
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
 
 	if (isset($_GET["friend"]) && $_GET["friend"] != $_SESSION["username"]) {
 		$self = false;
